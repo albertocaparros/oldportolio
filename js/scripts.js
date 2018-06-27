f289e2948aebeb96a57d63d73ac8bfa3f8f2061d
albertocaparros <acaparros87@gmail.com>
2018-06-27 13:26:50 +0900

    multilenguaje

--- js/scripts.js
+++ js/scripts.js
@@ -41,67 +41,90 @@ $(function () {
             return false; 
         });
 
-(function() {
-  var delay = false;
- 
-  $(document).on('mousewheel DOMMouseScroll', function(event) {
-    event.preventDefault();
-    if(delay) return;
+    (function () {
+        var delay = false;
 
-    delay = true;
-    setTimeout(function(){delay = false},50)
+        var idioma = $(".spanish").css("display");
 
-    var wd = event.originalEvent.wheelDelta || -event.originalEvent.detail;
+        $(document).on('mousewheel DOMMouseScroll', function (event) {
+            idioma = $(".spanish").css("display");
+            
+            if (idioma == "block") {
+                event.preventDefault();
+                if (delay) return;
 
-    var a= document.getElementsByClassName('scroll'); 
-    if(wd < 0) {
-      for(var i = 0 ; i < a.length ; i++) {
-        var t = a[i].getClientRects()[0].top;
-        if(t >= 40) break;
-      }
-    }
-    else {
-      for(var i = a.length-1 ; i >= 0 ; i--) {
-        var t = a[i].getClientRects()[0].top;
-        if(t < -20) break;
-      }
-    }
+                delay = true;
+                setTimeout(function () {
+                    delay = false
+                }, 50)
+
+                var wd = event.originalEvent.wheelDelta || -event.originalEvent.detail;
+
+                var a = document.getElementsByClassName('scrollES');
+                if (wd < 0) {
+                    for (var i = 0; i < a.length; i++) {
+                        var t = a[i].getClientRects()[0].top;
+                        if (t >= 40) break;
+                    }
+                } else {
+                    for (var i = a.length - 1; i >= 0; i--) {
+                        var t = a[i].getClientRects()[0].top;
+                        if (t < -20) break;
+                    }
+                }
+
+                if (i >= 0 && i < a.length) {
+                    $('html,body').animate({
+                        scrollTop: a[i].offsetTop
+                    }, 1000);
+                }
+            } else {
+                event.preventDefault();
+                if (delay) return;
+
+                delay = true;
+                setTimeout(function () {
+                    delay = false
+                }, 50)
+
+                var wd = event.originalEvent.wheelDelta || -event.originalEvent.detail;
+
+                var a = document.getElementsByClassName('scrollEN');
+                if (wd < 0) {
+                    for (var i = 0; i < a.length; i++) {
+                        var t = a[i].getClientRects()[0].top;
+                        if (t >= 40) break;
+                    }
+                } else {
+                    for (var i = a.length - 1; i >= 0; i--) {
+                        var t = a[i].getClientRects()[0].top;
+                        if (t < -20) break;
+                    }
+                }
+
+                if (i >= 0 && i < a.length) {
+                    $('html,body').animate({
+                        scrollTop: a[i].offsetTop
+                    }, 1000);
+                }
+            }
+        });
+
+    })();
     
-    if(i >= 0 && i < a.length) {
-        console.log(a.length);
-      $('html,body').animate({
-        scrollTop: a[i].offsetTop
-      }, 1000);
-    }
-  });
-})();
-    
-});
-
-/*
-$(document).ready(function () {
-
-    $('#boton-web').mouseenter(function () {
-        div = $('div.inicial');
-        divMostrar = $("div.web");
-
-        div.hide();
-        divMostrar.show(50);
-    }).mouseleave(function () {
-        div.show(50);
-        divMostrar.hide();
+    $(".idioma").click(function(){
+        var idioma = $(".spanish").css("display");
+        
+        if (idioma == "block") {
+            $(".spanish").css("display","none");
+            $(".english").css("display","block");
+        }
+        else{
+            $(".spanish").css("display","block");
+            $(".english").css("display","none");
+            
+        }
+        
     });
+}); // FIN JS
 
-    $('#boton-frameworks').mouseenter(function () {
-        div = $('div.inicial');
-        divMostrar = $("div.frameworks");
-
-        div.hide();
-        divMostrar.show(50);
-    }).mouseleave(function () {
-        div.show(50);
-        divMostrar.hide();
-    });
-
-});
-*/
